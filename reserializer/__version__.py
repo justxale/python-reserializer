0c1e8406210715f244c43741e55d35245eaac6e9
JustXale <alexev2401@gmail.com>
2022-11-12 19:53:50 +0300

    this thing

--- reserializer/__version__.py
+++ reserializer/__version__.py
@@ -1,11 +1,11 @@
 __version__ = "0.0.1"
-__supported_formats__ = (""""'json', 'xml',""" 'rsav', 'reon', 'reml')
+__supported_formats__ = ('jaon') # also will be added 'json', 'xml', 'rsav'
 __title__ = "reserializer"
-__description__ = "Python HTTP for Humans."
+__description__ = "Simplified data serialization package"
 __author__ = "JustXale"
 # __license__ = ""
 __copyright__ = "Copyright 2022 JustXale"
-__addons__ = [
-    'json_lib',
-    'xml_lib'
-]
+# __addons__ = [
+#     'json_lib',
+#     'xml_lib'
+# ] todo
